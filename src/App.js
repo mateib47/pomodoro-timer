dde333cc63dea7d45883449fc72cf37ec418d213
Matei Bucur <73275343+mateib47@users.noreply.github.com>
2022-06-27 23:57:44 +0200

    add state hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import "./app.scss";
+import TimerBox from "./components/TimerBox";
+import { useState, useEffect } from "react";
+import Topbar from "./components/Topbar/Topbar";
+
 
 const App = () => {
-  return (
-    <div className='app'>Pomodoro timer</div>
-  )
-}
 
-export default App
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(20);
+  const [hours, setHours] = useState(0);
+
+  useEffect(() => {
+   if (seconds <= 0){
+    setSeconds(59);
+    setMinutes(minutes-1):
+   }
+   
+}, [seconds]);
+
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="section">
+        <TimerBox
+          minutes={minutes}
+          seconds={seconds}
+          hours={hours}
+          setMinutes={setMinutes}
+          setSeconds={setSeconds}
+          setHours={setHours}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
